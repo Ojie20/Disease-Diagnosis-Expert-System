f91018b7ea9e8f61e3e6d5cf2ef5695bcdd60b96
Alexius <benedictosadolor@gmail.com>
2025-04-22 14:19:36 +0100

    initial project structure and env setup

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.config.from_pyfile('.env')
+
+@app.route('/')
+def index():
+    return render_template('layout.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
